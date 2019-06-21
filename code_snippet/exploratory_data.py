9987f08a5563e0dffd3af4f9e108dc28a2e8a4de
irfanchahyadi <irfanchahyadi@gmail.com>
2019-06-21 12:57:48 +0700

    Update exploratory_data.py

--- code_snippet/exploratory_data.py
+++ code_snippet/exploratory_data.py
@@ -10,5 +10,10 @@ print(df.groupby(['State']).sum())
 total = df.apply(np.sum, axis=0)
 cont = df.apply(lambda x: x/sum(x), axis=0)
 
+# PLOTTING
+hist = df.hist(bins=10)
+plot = df.plot()
+boxplot = df.boxplot()
+
 # PAIR PLOT
 pd.plotting.scatter_matrix(data)
