36552052250254474de95b5c3810024190a6e8cd
irfanchahyadi <irfanchahyadi@gmail.com>
2019-06-21 12:58:11 +0700

    Update feature_engineering.py

--- code_snippet/feature_engineering.py
+++ code_snippet/feature_engineering.py
@@ -1,1 +1,11 @@
+# COPY DATAFRAME
+dx = df[['col1', 'col2']].copy()
 
+# DROP COLUMN
+df.drop('col1', axis=1, inplace=True)
+
+# CREATE COLUMN
+df['newcol'] = 1
+
+# CREATE STD DEVIATION EACH COLUMN
+df.apply(lambda x: abs(x-x.mean())/x.std(), axis=0)
