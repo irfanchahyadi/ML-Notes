0ba422e72f33f214ff87de4eeed1fd58cf0cdb43
Irfanchahyadi <irfanchahyadi@gmail.com>
2019-08-17 18:02:36 +0700

    Update misc.py
    add regex

--- code_snippet/misc.py
+++ code_snippet/misc.py
@@ -13,3 +13,25 @@ pd.options.display.max_rows = len(df)	# default 60
 plt.rcParams['figure.figsize'] = (16, 10)
 plt.colormaps()		# return all possible cmap
 plt.cm.Reds			# Reds cmap
+
+# CHEATSHEET REGEX
+\d any number
+\D anything but number
+\w any character
+\W anything but character
+.  any character except newline
+\b whitespace
+\. dot character
++  match 1 or more
+*  match 0 or 1
+?  match 0 or 1
+^  start string
+$  end string
+{} expect 1-3, ex \d{3} expect 3 digit number, \d{1,3} expect 1-3 digit number
+[] range, ex [A-Z] expect all capital letter, [abcd] expect letter a, b, c, d
+|  either or, ex \d{1,4}|[A-z]{4,10} expect 1-4 digit number or 4-10 character word
+\n newline
+\s space
+\t tab
+\e escape
+\r return
