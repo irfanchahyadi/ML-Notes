e65353a71ea5f95bb5d3034a144d453fc63aa2d5
Irfanchahyadi <irfanchahyadi@gmail.com>
2019-10-26 20:55:59 +0700

    delete unnecessary file

--- code_snippet/train_model.py
+++
@@ -1,6 +0,0 @@
-# TRAIN TEST SPLIT
-X_train, X_test, y_train, y_test = model_selection.train_test_split(X, y, test_size=0.2)
-
-# TRAIN MODEL
-model = LinearRegression()
-model.fit(X_train, y_train)
